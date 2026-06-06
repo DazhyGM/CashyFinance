9843b393a18dfe75e4bab23371f91347b9d20d87
DazhyGM <kevinsabogal24@gmail.com>
2026-06-05 23:40:59 -0500

    edit vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,18 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+
   server: {
+    host: '0.0.0.0',
     port: 5173,
+
+    watch: {
+      usePolling: true
+    },
+
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://backend:3000',
         changeOrigin: true,
         secure: false
       }
